7bc9d035816814bbcfd3529cb1dfcf9e059cf99c
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-10-27 23:52:43 -0700

    v-model in inputfileds

--- data.js
+++ data.js
@@ -2,5 +2,11 @@ const cards = [
     {
         front: 'ways to add vue.js to a project',
         back: 'script tag (local or cdn )<br> install with npm <br> use vue-cli'
+    },{
+        front: 'ways to add vue.js to a project2',
+        back: 'script tag (local or cdn )<br> install with npm <br> use vue-cli'
+    },{
+        front: 'ways to add vue.js to a project3',
+        back: 'script tag (local or cdn )<br> install with npm <br> use vue-cli'
     }
 ]
